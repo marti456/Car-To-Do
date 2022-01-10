7223c547cc87433f27340498d7ba20820f9b2dcf
Stelian <steliantodorichkov03@gmail.com>
2022-01-10 22:25:55 +0200

    add login and registration form

---
+++ frontend/car-to-do/src/components/Registration.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Container, CssBaseline, Typography, Paper, Grid, TextField, Button } from '@mui/material'
+
+
+class Registration extends React.Component {
+    render() {
+        return (
+            <>
+                <CssBaseline />
+                <Container maxWidth="xs" style={{marginTop: "8%"}}>
+                    <Paper elevation={12} style={{padding: "8%"}}>
+                        <Typography variant="h4" color="textPrimary" align="center">
+                            Sign up
+                        </Typography>
+                        <hr />
+                        <br />
+                        <Grid container spacing={3} justify="center">
+                            <Grid item xs={12} sm={12} md={12}>
+                                <TextField
+                                    fullWidth
+                                    label="Username"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12}>
+                                <TextField
+                                    fullWidth
+                                    label="Password"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12}>
+                                <TextField
+                                    fullWidth
+                                    label="Confirm password"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12}>
+                                <Grid container justifyContent="center" >
+                                    <Grid item>
+                                        <Button variant="contained" color="success" size="large"  >Sign up</Button>
+                                    </Grid>  
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Container>
+            </>
+        );
+    }
+}
+
+export default Registration;
