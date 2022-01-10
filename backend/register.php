9808bb31f0bce364e3a352d3484c16c8eb7cd985
marti456 <martin.damyanov03@gmail.com>
2022-01-11 00:43:20 +0200

    Working connection between ReactJS and PHP

--- backend/register.php
+++ backend/register.php
@@ -10,21 +10,32 @@ $errors = array();
 $db = mysqli_connect('localhost', 'root', '', 'DB');
 
 // REGISTER USER
-if (isset($_POST['reg_user'])) {
+
+header("Access-Control-Allow-Origin: *");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+echo $_SERVER['REQUEST_METHOD'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  echo "In IF";
   // receive all input values from the form
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  //$username = isset($_POST['username'])?json_decode($_POST['username']):null; 
+  //$password_1 =isset($_POST['password'])?json_decode($_POST['password']):null; 
+  //$usernm = $_POST['username']
+  $username = mysqli_real_escape_string($db, $_POST["username"]);
+  //$email = mysqli_real_escape_string($db, $_POST['email']);
+  //$password = $_POST['password']
+  $password_1 = mysqli_real_escape_string($db, $_POST['password']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
+  //if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
-  }
+  //if ($password_1 != $password_2) {
+	//array_push($errors, "The two passwords do not match");
+  //}
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
@@ -32,6 +43,7 @@ if (isset($_POST['reg_user'])) {
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
+  echo "Added new user";
   if ($user) { // if user exists
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
