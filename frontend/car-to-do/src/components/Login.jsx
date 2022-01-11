72de50d189d4c78204b6c8facd9dfd7b2119799c
Stelian <steliantodorichkov03@gmail.com>
2022-01-11 23:16:27 +0200

    Login functional componennt

--- frontend/car-to-do/src/components/Login.jsx
+++ frontend/car-to-do/src/components/Login.jsx
@@ -1,88 +1,89 @@
-import React from "react";
-import { Container, CssBaseline, Typography, Paper, Grid, TextField, Button, Alert } from '@mui/material';
+import React, { useState } from "react";
+import { Container, CssBaseline, Typography, Paper, Grid, TextField, Button} from '@mui/material'
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+import Allerts from "./Allerts";
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props)
+const Login = (props) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [alert, setAlert] = useState('')
 
-        this.state = {
-            username: '',
-            password: '',
-            alert: ''
+    const navigate = useNavigate();
+
+    const handleChangeUsername = (event) => {
+        setUsername(event.target.value)
+    }
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const postData = async () => {
+        if (username === '' || password === '') {
+            setAlert('noAllData')
         }
-    }
-
-    handleChange = input => e => {
-        this.setState({[input]: e.target.value});
-    }
-
-    async postData() {
-        if (this.state.username === '' || this.state.password === '') {
-            this.setState({alert: 'noAllData'})
+        else if (password.length < 8) {
+            setAlert('shortPass')
         }
         else {
             const content = {
-                username: this.state.username,
-                password: this.state.password
+                username: username,
+                password: password
             }
             const response = await axios.post('http://localhost:8080/Car-To-Do/login.php', content)
             console.log(response)
-            window.location.href='/';
+            props.makeLogIn(username)
+            navigate('/')
         }
     }
 
-    renderAlert = () => {
-        if (this.state.alert === 'noAllData') {
-            return <Alert severity="error" variant="filled">
-                    The form must be completed!
-                </Alert>
-        }
-    }
+    
 
-    render() {
-        return (
-            <>
-                <CssBaseline />
-                <Container maxWidth="xs" style={{marginTop: "8%"}}>
-                    <Paper elevation={12} style={{padding: "8%"}}>
-                        <Typography variant="h4" color="textPrimary" align="center">
-                            Sign in
-                        </Typography>
-                        <hr />
-                        <br />
-                        <Grid container spacing={3} justify="center">
-                            <Grid item xs={12} sm={12} md={12}>
-                                {this.renderAlert()}
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12}>
-                                <TextField
-                                    fullWidth
-                                    label="Username"
-                                    onChange={this.handleChange('username')}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12}>
-                                <TextField
-                                    fullWidth
-                                    label="Password"
-                                    onChange={this.handleChange('password')}
-                                    type="password"
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12}>
-                                <Grid container justifyContent="center" >
-                                    <Grid item>
-                                        <Button variant="contained" color="success" size="large"  >Sign in</Button>
-                                    </Grid>  
-                                </Grid>
+    return (
+        <>
+            <CssBaseline />
+            <Container maxWidth="xs" style={{marginTop: "8%"}}>
+                <Paper elevation={12} style={{padding: "8%"}}>
+                    <Typography variant="h4" color="textPrimary" align="center">
+                        Sign in
+                    </Typography>
+                    <hr />
+                    <br />
+                    <Grid container spacing={3} justify="center">
+                        <Grid item xs={12} sm={12} md={12}>
+                            <Allerts alert={alert} />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12}>
+                            <TextField
+                                fullWidth
+                                label="Username"
+                                onChange={handleChangeUsername}
+                                value={username}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12}>
+                            <TextField
+                                fullWidth
+                                label="Password"
+                                onChange={handleChangePassword}
+                                value={password}
+                                type="password"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12}>
+                            <Grid container justifyContent="center" >
+                                <Grid item>
+                                    <Button variant="contained" color="success" size="large"  onClick={postData.bind(this)}>Sign in</Button>
+                                </Grid>  
                             </Grid>
                         </Grid>
-                    </Paper>
-                </Container>
-            </>
-        );
-    }
+                    </Grid>
+                </Paper>
+            </Container>
+        </>
+    );
 }
+
 
 export default Login;
