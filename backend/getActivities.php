e45122d1262c4baf71a8eb8d9fed193b792ad24a
marti456 <martin.damyanov03@gmail.com>
2022-04-17 20:37:55 +0300

    Updated getActivities.php

--- backend/getActivities.php
+++ backend/getActivities.php
@@ -4,7 +4,7 @@ session_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 $rest_json = file_get_contents("php://input");
-$_POST = json_decode($rest_json, true);
+$_GET = json_decode($rest_json, true);
 
 
 // initializing variables
@@ -14,10 +14,14 @@ $errors = array();
 $db = mysqli_connect('localhost', 'root', '', 'DB');
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $activities_check_query = "SELECT * FROM activities";
-    $result = mysqli_query($db, $activities_check_query) or die("database error:". mysqli_error($db));
+    $username = $_GET["username"];
+    $user_id_query = "SELECT id FROM users WHERE username = '$username' LIMIT 1";
+    $user_id_result = mysqli_query($db, $user_id_query);
+    $user_id = mysqli_fetch_array($user_id_result)['id'];
+    $activities_check_query = "SELECT * FROM activities WHERE user_id = '$user_id'";
+    $result = mysqli_query($db, $activities_check_query) or ($errors[] = mysqli_error($db));
     if (count($errors) == 0) {      
-    $activities = mysqli_fetch_all ($result, MYSQLI_ASSOC);
+    $activities = mysqli_fetch_all($result, MYSQLI_ASSOC);
     echo json_encode($activities);
     }
     else{
