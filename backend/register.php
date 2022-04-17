52d5252f19ab2e1fa14a3871bc2ab48e105c8d1d
marti456 <37450663+marti456@users.noreply.github.com>
2022-04-17 12:16:17 +0300

    Backend (#7)
    * Added login backend
    
    * Working connection between ReactJS and PHP
    
    * Repaired CORS policy
    
    * Updated registration and login in php

--- backend/register.php
+++ backend/register.php
@@ -1,96 +1,74 @@
 <?php
 session_start();
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+
 // initializing variables
 $username = "";
-$email    = "";
 $errors = array(); 
 
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'DB');
 
 // REGISTER USER
-if (isset($_POST['reg_user'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // receive all input values from the form
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $username = mysqli_real_escape_string($db, $_POST["username"]);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
+  //if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+  //if ($password_1 != $password_2) {
+  //array_push($errors, "The two passwords do not match");
+  //}
+
+ if (count($errors) > 0){
+    foreach ($errors as $error) {
+      echo $error . "\n";
+    }
   }
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
+  
+ 
+
+
   if ($user) { // if user exists
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
     }
-
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
-    }
+    echo "Username already exists";
   }
 
   // Finally, register user if there are no errors in the form
-  if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  else if (count($errors) == 0) {
+    $password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+    $query = "INSERT INTO users (username, password) 
+          VALUES('$username', '$password')";
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+    //header('location: index.php');
+
+    echo "Added new user";
+  }
+
+  else {
+    echo "Error";
   }
 }
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <div class="header">
-    <h2>Register</h2>
-  </div>
   
-  <form method="post" action="register.php">
-    <div class="input-group">
-      <label>Username</label>
-      <input type="text" name="username" value="<?php echo $username; ?>">
-    </div>
-    <div class="input-group">
-      <label>Email</label>
-      <input type="email" name="email" value="<?php echo $email; ?>">
-    </div>
-    <div class="input-group">
-      <label>Password</label>
-      <input type="password" name="password_1">
-    </div>
-    <div class="input-group">
-      <label>Confirm password</label>
-      <input type="password" name="password_2">
-    </div>
-    <div class="input-group">
-      <button type="submit" class="btn" name="reg_user">Register</button>
-    </div>
-    <p>
-      Already a member? <a href="login.php">Sign in</a>
-    </p>
-  </form>
-</body>
-</html>
+?>
