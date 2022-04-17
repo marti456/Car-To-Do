34c3fe464e0a1f0256ea89ca4f6656ebbb709ee9
marti456 <martin.damyanov03@gmail.com>
2022-04-17 21:21:40 +0300

    Added deleteActivity.php

---
+++ backend/deleteActivity.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $activity_id = mysqli_real_escape_string($db, $_POST["activity_id"]);
+    //DELETE FROM table_name WHERE condition;
+    $user_id_query = "DELETE FROM activities WHERE id = '$activity_id'";
+    $user_id_result = mysqli_query($db, $user_id_query);
+    echo "Activity deleted successfully";
+}
+?>
--- backend/getActivities.php
+++ backend/getActivities.php
@@ -13,7 +13,7 @@ $errors = array();
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'DB');
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = mysqli_real_escape_string($db, $_POST["username"]);
     $user_id_query = "SELECT id FROM users WHERE username = '$username' LIMIT 1";
     $user_id_result = mysqli_query($db, $user_id_query);
@@ -21,8 +21,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $activities_check_query = "SELECT * FROM activities WHERE user_id = '$user_id'";
     $result = mysqli_query($db, $activities_check_query) or ($errors[] = mysqli_error($db));
     if (count($errors) == 0) {      
-    $activities = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    echo json_encode($activities);
+        $activities = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        echo json_encode($activities);
     }
     else{
         echo "No activities";
